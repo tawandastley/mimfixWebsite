a4025feea5bb28a2add3727cea2283bd24a40c60
Tawanda Chandiwana <astleytac@gmail.com>
2023-11-15 14:51:20 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
