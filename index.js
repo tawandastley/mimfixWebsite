d4588578552e915bcd3c02e55d40700515c4fb7b
Tawanda Chandiwana <astleytac@gmail.com>
2023-11-18 07:06:20 +0200

    added more styling

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
 });
 
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/pricing", (req, res) => {
+    res.render("cover");
+})
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
